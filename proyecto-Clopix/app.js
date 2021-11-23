67400435fd286e5b1a689dd4ec84442e11358f22
Rociobohn <rocioquinterosbohn@gmail.com>
2021-11-23 15:12:19 -0300

    actualizo registro2

--- proyecto-Clopix/app.js
+++ proyecto-Clopix/app.js
@@ -8,7 +8,7 @@ app.listen (process.env.PORT ||3000, ()=>{
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/login', (req,res)=>{
