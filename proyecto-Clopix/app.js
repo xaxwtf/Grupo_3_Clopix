a32240d6af9fbf1a41f24465a0d92de80980b709
Rociobohn <rocioquinterosbohn@gmail.com>
2021-11-23 22:12:18 -0300

    actualizo home y otros

--- proyecto-Clopix/app.js
+++ proyecto-Clopix/app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen (process.env.PORT ||3000, ()=>{
+app.listen (process.env.PORT ||3001, ()=>{
     console.log('Servidor funcionando bien');
 });
 
