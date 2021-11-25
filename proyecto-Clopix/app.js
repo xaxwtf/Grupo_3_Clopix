8b5d38c9ef65ebb538e8ae10811e749b10f9ae02
Rociobohn <rocioquinterosbohn@gmail.com>
2021-11-25 11:52:54 -0300

    actualizo para heroku

--- proyecto-Clopix/app.js
+++ proyecto-Clopix/app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen (process.env.PORT ||3002, ()=>{
+app.listen (process.env.PORT ||3001, ()=>{
     console.log('Servidor funcionando bien');
 });
 
