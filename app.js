bb21b8d91a87d6f13ca27760b7fcc8ba6ba5eaa8
S_Ariel <xaxwtf@gmail.com>
2021-12-23 12:37:15 -0300

    intentando hacer funcionar herok

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { login } = require('./src/controllers/userCtrl');
 const app = express();
 const methodOverride= require("method-override");
 ///cargando rutas
