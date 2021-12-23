6bc7a7a037616a7a06b171995023342c02fd5e21
S_Ariel <xaxwtf@gmail.com>
2021-12-23 12:26:27 -0300

    borrando coments

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,20 +3,4 @@ const home=require('../controllers/mainCtrl');
 const rutas=express.Router();
 rutas.get('/', home.mostrar);
 
-/*
-rutas.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/src/views/login.html');
-});
-
-rutas.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/src/views/register.html');
-});
-
-rutas.get('/Catalogo', (req,res)=>{
-    res.sendFile(__dirname + '/src/views/productDetail.html');
-});
-
-rutas.get('/register2', (req,res)=>{
-    res.sendFile(__dirname + '/src/views/register2.html');
-});*/
 module.exports=rutas;
