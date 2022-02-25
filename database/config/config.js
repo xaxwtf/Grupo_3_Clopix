f1e24b5224ffd35f9512b4737a933ce2c9934bd2
S_Ariel <xaxwtf@gmail.com>
2022-02-24 21:59:04 -0300

    base de datos conectada

--- database/config/config.js
+++ database/config/config.js
@@ -1,25 +1,20 @@
-module.exports=
+require('dotenv').config()
+module.exports =
 {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "clopix_db",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+    "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+},
+    "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
 }
-
+}
